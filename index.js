20fd4c73cf746f6d84026b154c50859f7200d358
jeremyam <jeremy@jeremyam.com>
2024-06-19 16:10:17 -0600

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 const Sheets = require("./src/Sheets")
-const db = new Sheets({
-    developmentId: "DevID",
-    productionId: "ProdID",
-    serviceAccount: require("./storage/credentials.json"),
-})
-
-db.setMode({ development: true })
-
-console.log(db)
+module.exports = Sheets
