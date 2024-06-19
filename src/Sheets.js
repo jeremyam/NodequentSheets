fce8d6eb44f9e7fc8465cd3819bec9df5e7ce579
jeremyam <jeremy@jeremyam.com>
2024-06-19 16:00:41 -0600

    updated

---
+++ src/Sheets.js
@@ -0,0 +1,6 @@
+function Sheets({ developmentId: devId, productionId: prodId, serviceAccount: account }) {
+    this.devId = devId
+    this.prodId = prodId
+    // Service Account Credentials.
+    this.account = account
+}
